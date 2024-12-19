b0d08e06b7b8339027606d10bae891274521a392
NurKholis <kholis478@gmail.com>
2024-12-19 18:51:34 +0700

    add reviews

--- src/data/reviews.ts
+++ src/data/reviews.ts
@@ -3,23 +3,37 @@ import { ReviewType } from '../types';
 export const reviews: ReviewType[] = [
   {
     id: 'review1',
-    name: 'Budi Santoso',
+    name: 'EG`LEVI YT',
     rating: 5,
-    comment: 'Produk sangat enak dan segar! Pelayanannya juga ramah.',
+    comment: 'Jus nya enak, pelayan ramah Cuma kalo ngantri agak lama',
     product: 'product1'
   },
   {
     id: 'review2', 
-    name: 'Ani Wijaya',
-    rating: 4,
-    comment: 'Rasa makanannya luar biasa, harga terjangkau.',
+    name: 'suPPer21 slikur',
+    rating: 5,
+    comment: 'Ngejus pinggir jalan joss bgt..',
     product: 'product2'
   },
   {
     id: 'review3',
-    name: 'Rini Kurniawan',
+    name: 'vloge Ezi',
     rating: 5,
-    comment: 'Selalu menjadi pilihan utama saya untuk camilan.',
+    comment: 'Langganan paten ini mah... 🥰😍😍😍😍 Pilihan menu vegetarian: Menu buah buahan salada jus',
     product: 'product3'
+  },
+  {
+    id: 'review3',
+    name: 'Rusman Alii',
+    rating: 5,
+    comment: 'juss nya sangat fresh . penjaga nya sangat ramah tidak suka marah marah',
+    product: 'product4'
+  },
+  {
+    id: 'review3',
+    name: 'Nur Kholis',
+    rating: 5,
+    comment: 'Jus paling enak di haurgeulis..... Recomended banget Murah Murah Lagii 🥰😍😍 ',
+    product: 'product5'
   }
 ];
