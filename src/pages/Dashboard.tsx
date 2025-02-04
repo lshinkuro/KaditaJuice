331e870284a21b34289ebbd1124a8b66b0b7a5fb
NurKholis <kholis478@gmail.com>
2025-02-05 00:06:40 +0700

    change env

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -37,12 +37,12 @@ const Dashboard: React.FC = () => {
 
             {/* Sidebar untuk Desktop dan Mobile */}
             <div
-                className={`
-          fixed inset-y-0 left-0 z-50 transform transition-transform duration-300 ease-in-out
-          md:static md:block md:translate-x-0
-          ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}
-          w-64 bg-gray-800 text-white
-        `}
+                className ={`
+                fixed inset-y-0 left-0 z-50 transform transition-transform duration-300 ease-in-out
+                md:static md:block md:translate-x-0
+                ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}
+                w-64 bg-gray-800 text-white
+                `}
             >
                 <Sidebar
                     currentPage={currentPage}
